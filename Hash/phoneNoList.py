7e9b877e9db76de7ed82d58b1bc7998b8c7c5285
dev-jsoh <59411718+dev-jsoh@users.noreply.github.com>
2020-01-07 11:05:33 +0900

    update PhoneNoList.py

---
+++ Hash/phoneNoList.py
@@ -0,0 +1,20 @@
+def solution(phone_book):
+    answer = True
+    answer = clsfy(phone_book)
+    return answer
+
+def clsfy(nums):
+    tmp = [[] for _ in range(10)]
+    for i in nums:
+        tmp[int(i[0])].append(i[1:])
+    nxt = []
+    for i in tmp:
+        if len(i) >= 2:
+            nxt.append(i)
+    if len(nxt) == 0:
+        return True
+    for n in nxt:
+        if '' in n:
+            return False
+        else:
+            return clsfy(n)
